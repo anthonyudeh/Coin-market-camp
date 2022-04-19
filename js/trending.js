1982fa138d98b2e00fe9b23c6f2b15b046d2d6f3
Innocent <ekweozorinnocent@gmail.com>
2022-04-19 20:03:20 +0100

    adding js

--- js/trending.js
+++ js/trending.js
@@ -11,11 +11,29 @@ const getTrending = async () => {
     );
 
     console.log(response);
-    
+
   } catch (errors) {
     console.error(errors);
   }
 }
 getTrending()
+
+// const getTrending = async () => {
+//   //   spinner.style.display = "block";
+//   try {
+//     const response = await window.axios.get(
+//       "https://cors-any-where-code-mena.herokuapp.com/https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/historical",
+//       {
+//         headers: {
+//           "X-CMC_PRO_API_KEY": "837459ce-5242-457a-82c9-af52040c2776",
+//         },
+//       }
+//     );
+
+//     console.log(response);
+//   } catch (errors) {
+//     console.error(errors);
+//   }
+// };
 
 // https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/historical
